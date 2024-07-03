4423bdb5fc2174bd7376cd69183c4862effa135d
Percy Tuncar <33427262+PercyTuncar@users.noreply.github.com>
2024-07-03 13:31:58 -0500

    next-app-template

---
+++ app/layout.js
@@ -0,0 +1,61 @@
+import "@/styles/globals.css";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Navbar } from "@/components/navbar";
+
+export const metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex flex-col h-screen">
+            <Navbar />
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              {children}
+            </main>
+            <footer className="w-full flex items-center justify-center py-3">
+              <Link
+                isExternal
+                className="flex items-center gap-1 text-current"
+                href="https://tuncar.dev"
+                title="nextui.org homepage"
+              >
+                <span className="text-default-600">Desarrollado por</span>
+                <p className="text-primary">tuncar.dev</p>
+              </Link>
+            </footer>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ config/site.js
@@ -0,0 +1,68 @@
+export const siteConfig = {
+  name: "Next.js + NextUI",
+  description: "Realiza hermosos sitios web sin esfuerzo con NextUI.",
+  navItems: [
+    {
+      label: "Inicio",
+      href: "/",
+    },
+    {
+      label: "Documentación",
+      href: "/docs",
+    },
+    {
+      label: "Precios",
+      href: "/pricing",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "Acerca de",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Perfil",
+      href: "/profile",
+    },
+    {
+      label: "Tablero",
+      href: "/dashboard",
+    },
+    {
+      label: "Proyectos",
+      href: "/projects",
+    },
+    {
+      label: "Equipo",
+      href: "/team",
+    },
+    {
+      label: "Calendario",
+      href: "/calendar",
+    },
+    {
+      label: "Configuración",
+      href: "/settings",
+    },
+    {
+      label: "Ayuda y Retroalimentación",
+      href: "/help-feedback",
+    },
+    {
+      label: "Cerrar sesión",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/PercyTuncar",
+    twitter: "https://x.com/percy_tuncar",
+    docs: "https://nextui.org",
+    sponsor: "https://github.com/PercyTuncar",
+  },
+};
+
+export const SiteConfig = siteConfig;
